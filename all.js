22fd25c9b811acee352944cf804fbd78354f7fed
MattSn0w <80181525+MattSn0w1@users.noreply.github.com>
2021-09-19 18:36:20 -0400

    update a few things for the site

--- all.js
+++ all.js
@@ -1,41 +1,51 @@
 //navbar handler
 function topnav() {
-var x = document.getElementById("navLinks");
-var ico = document.getElementById("ico");
-if (x.style.display === "block") {
-  x.style.display = "none";
-  ico.innerText = "menu";
-  } else {
-  x.style.display = "block";
-  ico.innerText = "close";
-  }
+    var x = document.getElementById("navLinks");
+    var y = document.getElementById("menu");
+    var z = document.getElementById("nav")
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.classList.remove("material-icons-round")
+        y.innerText = "menu";
+        y.classList.remove("underline");
+        y.style.fontSize = "19px";
+        y.style.width = "50px";
+        y.style.borderRadius = "13px"
+        y.style.margin = "22px";
+        y.style.padding = "10px";
+        y.classList.add('frostedglass');
+        y.style.backgroundColor = "rgba(--blackbg .5)";
+        z.classList.remove('frostedglass');
+        z.style.height = "0";
+    } else {
+        x.style.display = "block";
+        y.classList.add("material-icons-round");
+        y.innerText = "close";
+        y.classList.add("underline");
+        y.style.fontSize = "24px";
+        y.style.padding = "30px";
+        y.style.margin = "0"
+        y.style.backgroundColor = "rgba(--blackbg .5)";
+        x.style.paddingTop = "0";
+        y.style.borderRadius = "0";
+        z.style.backgroundColor = "rgba(--blackbg .5)";
+        z.style.width = "100%";
+        z.style.height = "100%";
+        y.classList.remove("frostedglass");
+        z.classList.add('frostedglass');
+    }
 }
-//music widget
 async function init() {
-  //change the url with your user and API key
-  const json = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&page=1&nowplaying="true"&user=mattstar45&api_key=ea951fd34f50fdaf09f0c8e7bc8f184d&format=json');
-  //assign output as recenttracks so we can use it
-  let {
-    recenttracks
-  } = await json.json();
-  //check if the url is diff
-  //only run the other ifs if the url is dif
-  if (document.getElementById("url") != recenttracks.track[0].url) {
-    //at this point, we are specifying the inner html stuff
-    //href url for art
-    document.getElementById("url").href = recenttracks.track[0].url;
-    //album art
-    document.getElementById("album").src = recenttracks.track[0].image[3]["#text"];
-    //trackname
-    if (document.getElementById("trackname") != recenttracks.track[0]["name"]) {
-      document.getElementById("trackname").innerText = recenttracks.track[0]["name"];
+    const json = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&page=1&nowplaying="true"&user=mattstar45&api_key=ea951fd34f50fdaf09f0c8e7bc8f184d&format=json');
+    let { recenttracks } = await json.json();
+    if (document.getElementById("url") != recenttracks.track[0].url) {
+
+        document.getElementById("url").href = recenttracks.track[0].url;
+        document.getElementById("album").src = recenttracks.track[0].image[3]["#text"];
+
+        if (document.getElementById("trackname") != recenttracks.track[0]["name"]) { document.getElementById("trackname").innerText = recenttracks.track[0]["name"]; }
+        if (document.getElementById("artist") != recenttracks.track[0].artist["#text"]) { document.getElementById("artist").innerText = `${recenttracks.track[0].artist["#text"]} - ${recenttracks.track[0].album["#text"]}`; }
+
     }
-    //artist & album title 
-    if (document.getElementById("artist") != recenttracks.track[0].artist["#text"]) {
-      document.getElementById("artist").innerText = `${recenttracks.track[0].artist["#text"]} - ${recenttracks.track[0].album["#text"]}`;
-    }
-  }
-  //return a timeout so we can reload this
-  return setTimeout(init, 3000);
-  //tune the time if you want it quicker or slower (don't reccomend making it quicker)
+    setTimeout(init, 3000);
 }
